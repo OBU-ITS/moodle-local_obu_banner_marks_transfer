2ab4cf914033e91adb81773763b262270ca02a2a
Emir Kamel <p0092396@brookes.ac.uk>
2024-09-27 14:03:26 +0100

    adding very basic default files

---
+++ settings.php
@@ -0,0 +1,31 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    local_obu_banner_marks_transfer
+ * @author     Emir Kamel
+ * @copyright  2024, Oxford Brookes University {@link http://www.brookes.ac.uk/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage(get_string('pluginname', 'local_obu_banner_marks_transfer'), get_string('plugintitle', 'local_obu_banner_marks_transfer'));
+    $ADMIN->add('localplugins', $settings);
+    $settings->add(new admin_setting_configcheckbox('local_obu_banner_marks_transfer/enable', get_string('enable', 'local_obu_banner_marks_transfer'), get_string('enabledescription', 'local_obu_banner_marks_transfer'), 0));
+}
