52452294a752ebec727379df87c5998eab4c0952
Emir Kamel <p0092396@brookes.ac.uk>
2025-01-27 11:12:33 +0000

    version update

--- version.php
+++ version.php
@@ -23,12 +23,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024111802;
+$plugin->version = 2025012701;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_banner_marks_transfer';
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->release = 'v0.0.1';
+$plugin->release = 'v0.1.0';
 $plugin->dependencies = array(
     'enrol_ethos' => 2025011701,
 );
