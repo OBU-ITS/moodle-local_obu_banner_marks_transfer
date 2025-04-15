89ba8b537c7c364a3264ffa46a5ad318860ca921
Emir Kamel <p0092396@brookes.ac.uk>
2025-04-15 12:51:04 +0100

    more verbose trace logging for ethos object when created

--- classes/services/marks_transfer_service.php
+++ classes/services/marks_transfer_service.php
@@ -128,6 +128,7 @@ class marks_transfer_service {
         try {
             $marks_transfer_ethos_object = $this->prepare_marks_transfer_ethos_object($trace, $assessment_with_grade_logs);
             var_dump($marks_transfer_ethos_object);
+            $trace->output(print_r($marks_transfer_ethos_object, true));
         } catch (\Exception $e) {
             $trace->output("Failed to create marks transfer ethos object for {$assessment_with_grade_logs->access_restriction_group_idnum}: " . $e->getMessage());
             return;
