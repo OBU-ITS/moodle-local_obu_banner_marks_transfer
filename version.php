8f978e5647c2034f0a3feebc30856fed21794ced
Jock Coats <p0071665@brookes.ac.uk>
2025-04-29 12:55:04 +0100

    Update version and refine status handling in marks transfer
    Incremented the plugin version to 2025042901 and adjusted the release to v0.1.8. Refined the retrieval of 'Needs review' status using a more precise SQL comparison.

--- version.php
+++ version.php
@@ -23,12 +23,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025041501;
+$plugin->version = 2025042901;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_banner_marks_transfer';
 $plugin->maturity = MATURITY_STABLE;
 
-$plugin->release = 'v0.1.7';
+$plugin->release = 'v0.1.8';
 $plugin->dependencies = array(
     'enrol_ethos' => 2025022001,
 );
